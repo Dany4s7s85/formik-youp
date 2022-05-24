2bed656a64a828a8a8dfd0955e90f8a0bc074c37
Syed Ahsan Ali <syedahsanali995@gmail.com>
2022-05-24 00:25:42 -0700

    done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,100 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useRef } from 'react';
+import { Formik, ErrorMessage, Field } from "formik";
+import { Form } from 'react-bootstrap';
+import Button from 'react-bootstrap/Button';
+import * as yup from "yup";
+import "./App.css"
 
 function App() {
+  var myfocus = useRef(null)
+  
+  const handleSubmit = (values) => {
+    console.log(values)
+  };
+
+
+  useEffect(()=>{
+    myfocus.current.focus()
+  },[])
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Formik
+      validationSchema={CreatePasswordSchema}
+      onSubmit={handleSubmit}
+      initialValues={{
+        email: '',
+        password: '',
+      }}
+    >
+      {({
+        handleSubmit,
+        handleChange,
+        values,
+        errors,
+        touched,
+        handleBlur
+      }) => (
+
+        <Form noValidate onSubmit={handleSubmit}>
+          <div  className="form-input-wrap">
+            <Form.Label >Email</Form.Label>
+            <input
+              type="email"
+              name="email"
+              ref={myfocus}
+              placeholder="Enter Email"
+              value={values.email}
+              onChange={handleChange}
+              onBlur={handleBlur}
+              ref={myfocus}
+              isInvalid={!!errors.email && !!touched.email} />
+            {/* <Form.Control.Feedback type="invalid">
+              {errors.email}
+            </Form.Control.Feedback> */}
+            <ErrorMessage
+              component="div"
+              name="email"
+              className="error"
+            />
+          </div>
+          <div className="form-input-wrap">
+            <Form.Label>Password</Form.Label>
+            {/* <Form.Control type="password" placeholder="Your Password" className="input-item" /> */}
+            <Field
+              type="password"
+              name="password"
+              placeholder="enter password"
+              value={values.password}
+              className=""
+              onBlur={handleBlur}
+              onChange={handleChange}
+              isInvalid={!!errors.password && !!touched.password} />
+            {/* <Form.Control.Feedback type="invalid">
+              {errors.password}
+            </Form.Control.Feedback> */}
+            <ErrorMessage
+              component="div"
+              name="password"
+              className="error"
+            />
+          </div>
+
+          <div className="button-wrap">
+            <Button type="submit" className="w-100">Continue</Button>
+          </div>
+        </Form>
+      )}
+    </Formik>
   );
 }
+
+const CreatePasswordSchema = yup.object().shape({
+  email: yup.string().email('invalid email').required('email required'),
+  password:yup.string().required('password required')
+  .min(8, 'Password is too short - should be 8 chars minimum.')
+  .matches(/^(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]/, 'Password must include one upper case, and at least one number.'),
+});
 
 export default App;
